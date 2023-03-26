d9ca92d0a98b98e2c6d2c21d2c2582e55b7ed00a
Jacob Clouse <clouse.jp@gmail.com>
2023-03-26 14:58:34 -0400

    created snippet that will get image header info from file

--- _Code/Drafts/Read_Header_bmp.py
+++ _Code/Drafts/Read_Header_bmp.py
@@ -1,2 +1,7 @@
 # https://stackoverflow.com/questions/47003833/how-to-read-bmp-file-header-in-python
+# WARNING: DeprecationWarning: 'imghdr' is deprecated and slated for removal in Python 3.13 for import imghdr
 
+import imghdr
+
+bitmap_filename = input("Please input the name of the file you want to get the header from: ")
+print(imghdr.what(bitmap_filename))
