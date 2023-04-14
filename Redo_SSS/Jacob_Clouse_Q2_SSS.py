b02a580ac4519026a5845cb3dceb48e788d7f8e3
jacobpclouse <clouse.jp@gmail.com>
2023-04-14 10:14:58 -0400

    got a version working without numpy

--- Redo_SSS/Jacob_Clouse_Q2_SSS.py
+++ Redo_SSS/Jacob_Clouse_Q2_SSS.py
@@ -15,219 +15,89 @@ Required shares to retrieve the original image = 2
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # Importing Libraries / Modules 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-import numpy as np
 import random
 from PIL import Image
-# import sys
+
 
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # Functions
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+# --- Function to split an image into shares using Shamir's Secret Sharing algorithm --- 
+def read_image(path):
+    img = Image.open(path).convert('L')
+    # img.show()
+    img_array = []
+    width, height = img.size
+    for y in range(height):
+        for x in range(width):
+            img_array.append(img.getpixel((x, y)))
+    return img_array, (width, height)
+
+
 # --- Function to read in an image and adjust to only 250 max values --- 
 # SOURCE: http://paulbourke.net/dataformats/bitmaps/
-def max_250(origImage,newOutputName):
-    # Open the image file
-    image = Image.open(origImage)
-
-    # Convert the image to grayscale
-    image = image.convert("L")
-
-    # Get the pixel data
-    pixels = list(image.getdata())
-
-    # Modify the pixel values
-    new_pixels = [min(pixel, 250) for pixel in pixels]
-
-    # Create a new image object with the modified pixel values
-    new_image = Image.new(image.mode, image.size)
-    new_image.putdata(new_pixels)
-
-    # Save the new image as a bitmap file
-    new_image.save(newOutputName)
+def set_image_max_pixel_value(path, max_value=None):
+    img = Image.open(path).convert('L')
+    # img.show()
+    img_array = []
+    width, height = img.size
+    for y in range(height):
+        for x in range(width):
+            pixel = img.getpixel((x, y))
+            if max_value is not None:
+                pixel = min(pixel, max_value)
+            img_array.append(pixel)
+    return img_array, (width, height)
 
 
 
-# --- Function to split an image into shares using Shamir's Secret Sharing algorithm --- 
-def split_image_shamir(imageToEncrypt, requiredNumberShares, totalGenShares):
-    # Load the image, convert to grayscale just to be sure...
-    with Image.open(imageToEncrypt).convert('L') as youBoiImage:
-        dataArray = np.array(youBoiImage)
-    # return img_array.flatten(), img_array.shape
-    flat_boi = dataArray.flatten() # merge into single array
-    image_dimensions = dataArray.shape # ie should be like "640x480"
-    num_pix = (dataArray.flatten()).shape[0]
 
-    print(f"Your original image is {image_dimensions}")
-    print(f"You will create {totalGenShares} total")
-    print(f"You will need {requiredNumberShares} to retrieve")
-    print(f"Number of pixels: {num_pix}")
-
-
-    num_pixels = num_pix # Get the total number of pixels in the image
-    coef = np.random.randint(low = 0, high = 251, size = (num_pixels, requiredNumberShares - 1)) # Generate an array of random coefficients for the polynomial function
+def polynomial(img, n, r):
+    num_pixels = len(img)
+    coef = [[random.randint(0, 250) for _ in range(r-1)] for _ in range(num_pixels)]
     gen_imgs = []
-    for i in range(1, totalGenShares + 1):
-        base = np.array([i ** j for j in range(1, requiredNumberShares)]) # Generate the base for the polynomial function
-        base = np.matmul(coef, base) # Multiply the coefficients with the base to generate the shares
-        img_ = flat_boi + base # Add the shares to the original image
-        img_ = img_ % 251 # Take the modulo to ensure values are within the range [0, 250]
+    for i in range(1, n + 1):
+        base = [i ** j for j in range(1, r)]
+        img_ = []
+        for p in range(num_pixels):
+            base_sum = sum([coef[p][j] * base[j] for j in range(r-1)])
+            img_.append((img[p] + base_sum) % 251)
         gen_imgs.append(img_)
+    return gen_imgs
 
 
-    '''THIS CODE NEEDS TO CHANGE!!!'''
+def lagrange(x, y, num_points, x_test):
+    l = [0] * num_points
+    for k in range(num_points):
+        l[k] = 1
+        for k_ in range(num_points):
+            if k != k_:
+                l[k] = l[k] * (x_test - x[k_]) / (x[k] - x[k_])
+            else:
+                pass
+    L = 0
+    for i in range(num_points):
+        L += y[i] * l[i]
+    return L
 
-    coeffConverted = np.array(gen_imgs) # redo and merge with below
-    encryptedShares = coeffConverted.reshape(requiredNumberShares, *image_dimensions)
-    for i, outputImage in enumerate(encryptedShares):
-        Image.fromarray(outputImage.astype(np.uint8)).save("share_{}.bmp".format(i + 1))
 
-
-
-
-
-'''
-    # THIS IS NOT WORKING CORRECTLY ---
-    # Split the image data into k shares using Shamir's Secret Sharing algorithm
-    x_values = list(range(1, n+1))
-    y_values = [img_data[i::3] for i in range(3)]
-    shares = []
-    for y in y_values:
-        coeffs = [random.randint(1, 255) for i in range(k-1)]
-        coeffs.insert(0, int.from_bytes(y, byteorder='big'))
-        share = [(x, sum(c * x**i for i, c in enumerate(coeffs)) % 256) for x in x_values]
-        shares.append(share)
-
-    # Save each share as a separate BMP image file
-    for i, share in enumerate(shares):
-        img_share_data = np.zeros_like(img_data)
-        for j, (x, y) in enumerate(share):
-            img_share_data[j::n*3] = y
-        img_share_bytes = header + bytes(img_share_data)
-        with open(f"image_share_{i}.bmp", "wb") as f:
-            f.write(img_share_bytes)
-'''
+def decode(imgs, index, r, n):
+    assert len(imgs) >= r
+    x = index
+    dim = len(imgs[0])
+    img = []
+    for i in range(dim):
+        y = [imgs[j][i] for j in range(r)]
+        pixel = lagrange(x, y, r, 0) % 251
+        img.append(pixel)
+    return img
 
 
 
 
 
-# # --- Function to generate shares ---
-# # SOURCE: https://www.geeksforgeeks.org/implementing-shamirs-secret-sharing-scheme-in-python/#
-# def generate_shares(secret, n, k):
-#     """
-#     Generate n shares of the secret using a 2-out-of-n Shamir Secret Sharing scheme,
-#     where any k shares can be used to reconstruct the secret.
-#     Returns a list of tuples, where each tuple is a share in the form (x, y).
-#     """
-#     if k > n:
-#         raise ValueError("k must be less than or equal to n")
-    
-#     coefficients = [secret] + [random.randint(1, 2**32-1) for _ in range(k-1)]
-#     shares = []
-#     for i in range(n):
-#         x = i + 1
-#         y = sum([coefficients[j] * x**j for j in range(k)])
-#         shares.append((x, y))
-#     return shares
-
-
-
-# # --- Function to get shares and put them back together ---
-# # SOURCE: https://www.geeksforgeeks.org/implementing-shamirs-secret-sharing-scheme-in-python/#
-# def reconstruct_secret(shares):
-#     """
-#     Reconstruct the secret from a list of shares using Lagrange interpolation.
-#     Returns the reconstructed secret.
-#     """
-#     if len(shares) < 2:
-#         raise ValueError("At least 2 shares are required to reconstruct the secret")
-#     x_values, y_values = zip(*shares)
-#     secret = 0
-#     for i in range(len(shares)):
-#         numerator, denominator = 1, 1
-#         for j in range(len(shares)):
-#             if i != j:
-#                 numerator *= -x_values[j]
-#                 denominator *= (x_values[i] - x_values[j])
-#         secret += y_values[i] * numerator // denominator
-#     return secret
-
-
-# # --- Function to save shares as bitmaps ---
-# def save_shares_as_bitmaps(shares, header_data):
-#     for i, share in enumerate(shares):
-#         x, y = share
-#         binary_data = format(y, "08b") # convert the y value to a binary string
-#         byte_data = [int(binary_data[i:i+8], 2) for i in range(0, len(binary_data), 8)] # split the binary string into bytes
-#         share_data = bytearray(byte_data) # convert the bytes to a bytearray
-        
-#         # # update the header information with the share data length
-#         # share_size = len(share_data)
-#         # header_data = header_data[:2] + share_size.to_bytes(4, byteorder="little") + header_data[10:]
-
-#         # create a new bitmap file for the share
-#         share_path = f"share_{i+1}.bmp"
-#         with open(share_path, "wb") as f:
-#             f.write(header_data)
-#             f.write(share_data)
-
-
-
-# # --- Function open the image and get shares --- #
-# def encrypt_image(image_path, n, k):
-#     """
-#     Encrypt an image using Shamir Secret Sharing.
-#     Returns a list of n shares.
-#     """
-#     with open(image_path, "rb") as f:
-#         header_data = f.read(54) # read the bitmap header information
-#         image_data = f.read()
-#     # print(image_data)
-#     binary_data = "".join(format(byte, "08b") for byte in image_data)
-#     # go through each and every byte other than data and if its greater than 
-#     # print(binary_data)
-#     shares = generate_shares(int(binary_data, 2), n, k)
-#     return shares,header_data
-
-
-
-
-# # --- Function open shares and get tuples back --- #
-# def read_shares_from_bitmaps(num_shares):
-#     shares = []
-#     for i in range(1, num_shares+1):
-#         share_path = f"share_{i}.bmp"
-#         with open(share_path, "rb") as f:
-#             # skip the header information
-#             f.seek(54)
-#             share_data = f.read()
-
-#         # convert the share data to a binary string
-#         binary_data = "".join(format(byte, "08b") for byte in share_data)
-#         # convert the binary string to an integer
-#         y = int(binary_data, 2)
-#         x = i
-#         shares.append((x, y))
-
-#     return shares
-
-
-
-
-# # --- Function open the shares and return the image --- #
-# def decrypt_image(bitmap_shares):
-#     """
-#     Decrypt an image using Shamir Secret Sharing.
-#     Returns the decrypted image data as a bytes object.
-#     """
-#     secret = reconstruct_secret(bitmap_shares)
-#     binary_data = bin(secret)[2:]
-#     padding = 8 - len(binary_data) % 8
-#     binary_data = "0" * padding + binary_data
-#     bytes_data = bytes(int(binary_data[i:i+8], 2) for i in range(0, len(binary_data), 8))
-#     return bytes_data
 
 
 # --- Function to print out my Logo ---
@@ -256,8 +126,22 @@ new250Image = 'Only250.bmp'
 
 # myLogo()
 
-max_250(inputImage,new250Image)
-split_image_shamir(new250Image, requiredSharesToRetrieve, totalNumberOfShares)
+if __name__ == "__main__":
+    path = "1.bmp"
+    n = 5
+    r = 3
+    # img_flattened, shape = read_image(path)
+    img_flattened, shape = set_image_max_pixel_value(path,max_value=250)
+    gen_imgs = polynomial(img_flattened, n=n, r=r)
+    to_save = [Image.new("L", shape) for _ in range(n)]
+    for i, img in enumerate(gen_imgs):
+        to_save[i].putdata(img)
+        to_save[i].save("test2_{}.jpeg".format(i + 1))
+    origin_img = decode(gen_imgs[0:r], list(range(1, r + 1)), r=r, n=n)
+
+img = Image.new("L", shape, color=0)
+img.putdata(list(origin_img))
+img.save("test2_origin.jpeg")
 
 
 
